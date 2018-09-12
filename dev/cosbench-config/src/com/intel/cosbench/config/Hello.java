d8e3c3eb34df19008f689990e24648930c929a78
fuqiangy <1270440792@qq.com>
2018-09-12 14:37:23 +0800

    测试提交
    tijiao
    '

---
+++ dev/cosbench-config/src/com/intel/cosbench/config/Hello.java
@@ -0,0 +1,13 @@
+package com.intel.cosbench.config;
+
+
+public class Hello {
+	private Storage storage;    
+	
+	public Storage getSyncStorage() {
+		return storage;
+	}
+	public void setSyncStorage(Storage storage) {
+		this.storage = storage;
+	}
+}
